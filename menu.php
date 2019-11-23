3105c287ad6b312787c6f752a3a6812f18f5c926
Alexey Sergeev <alexey-sergeev@yandex.ru>
2019-11-23 23:22:27 +0400

    v 1.1

--- menu.php
+++ menu.php
@@ -16,14 +16,14 @@
         </div>
             
         <div class="col-12 col-sm-6 col-md-3 p-0 overflow-hidden">
-            <a href="?page=3" class="h5 text-decoration-none">
+            <a href="?page=12345" class="h5 text-decoration-none">
                 <img src="interface/summer.jpg" class="img-fluid">
                 <p class="p-5 m-0 text-center" style="background-color: #66C005;">Лето</p>
             </a>
         </div>
             
         <div class="col-12 col-sm-6 col-md-3 p-0 overflow-hidden">
-            <a href="?page=4" class="h5 text-decoration-none">
+            <a href="__empty__" class="h5 text-decoration-none">
                 <img src="interface/autumn.jpg" class="img-fluid">
                 <p class="p-5 text-center" style="background-color: #F9B652;">Осень</p>
             </a>
